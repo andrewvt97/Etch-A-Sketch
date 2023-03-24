cbaab4312d5486a30d556b0030c093fe26e2c2f8
Andrew <andrewvt97@gmail.com>
2023-03-23 20:24:50 -0400

    Add loop that creates div grid and hover features

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 
 const container = document.querySelector(".grid-container");
+const gridNumber = 16;
 
-for (let i = 0; i < 16 * 16; i++) {
-  const div = document.createElement("div");
-  div.backgroundColor = "green";
-  div.padding = "10px";
-  div.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "black";
-  });
-  container.appendChild(div);
+for (let i = 0; i < gridNumber; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < gridNumber; j++) {
+        const div = document.createElement("div");
+        div.setAttribute("style", "background-color: green; padding: 1%;")
+        div.classList.add("grid");
+        div.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+        });
+        row.appendChild(div);
+    }
+    container.appendChild(row);
 }
+
+// const p = document.createElement("p");
+// p.backgroundColor = "green";
+// p.textContent = "Hello World";
+// container.appendChild(p);
+
+// const div = document.createElement("div");
+
+// container.appendChild(div);
