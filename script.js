363c4e1ef47b0ea0728585b03b5c1cbb69a4e7e7
Andrew <andrewvt97@gmail.com>
2023-03-23 21:15:37 -0400

    Add logic to create new grid and remove old one
    Problem was fixed where I could not remove elements properly.
    The childElementsCount function is constantly changing so each time I
    tried to remove it it didn't work properly. Needed to set to variable
    first before deleting

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 
 const container = document.querySelector(".grid-container");
-const gridNumber = 16;
+let gridNumber = 16;
+const gridButton = document.querySelector(".grid-button");
+const gridSpace = 200;
 
-for (let i = 0; i < gridNumber; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < gridNumber; j++) {
-        const div = document.createElement("div");
-        div.setAttribute("style", "background-color: green; padding: 1%;")
-        div.classList.add("grid");
-        div.addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = "black";
-        });
-        row.appendChild(div);
+generateGrid(gridNumber);
+function generateGrid(gridNumber) {
+    for (let i = 0; i < gridNumber; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < gridNumber; j++) {
+            const div = document.createElement("div");
+            // div.setAttribute("style", `background-color: green; padding: ${gridSpace/gridNumber} px;`)
+            div.style.padding = `${gridSpace/gridNumber}px`;
+            div.classList.add("grid");
+            div.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = "black";
+            });
+            row.appendChild(div);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
-// const p = document.createElement("p");
-// p.backgroundColor = "green";
-// p.textContent = "Hello World";
-// container.appendChild(p);
 
-// const div = document.createElement("div");
+gridButton.addEventListener("click", (e) => {
+    // for (let i = 0; i < gridNumber; i++) {
+    //     container.removeChild(container.lastChild);
+    // }
+    gridNumber = prompt("How many squares per side?");
+    const count = container.childElementCount;
+    for (let i = 0; i < count; i++) { // this does not work because container.childElementCount is going down when you remove children
+        container.removeChild(container.lastChild);
+    }
+    generateGrid(gridNumber);
+});
 
-// container.appendChild(div);
