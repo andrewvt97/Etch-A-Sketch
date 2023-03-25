2703372dda484ad25254e49d66d4426462545621
Andrew <andrewvt97@gmail.com>
2023-03-24 20:02:05 -0400

    Reduce amount of variables and repetitive code

--- script.js
+++ script.js
@@ -1,86 +1,64 @@
-// removeEventListener problem was that I set currentTool as a string, giving only function name
+// removeEventListener problem was that I set currentTool as a string, giving only function name not entire function
+
+// variables
 const container = document.querySelector(".grid-container");
 let slider = document.querySelector(".slider");
 let gridNumber = slider.value;
+const gridSpace = 200; // amount of space for the grid
 
+// buttons
 const clearButton = document.querySelector(".clear-button");
 const rainbowButton = document.querySelector(".rainbow-button");
 const grayScaleButton = document.querySelector(".grayscale-button");
 const eraseButton = document.querySelector(".erase-button");
 const gridColorButton = document.querySelector(".grid-color-button")
+const changeColorButton = document.querySelector(".color-button");
+
+// color pickers
 let gridColorPicker = document.querySelector('#grid-color-picker');
 let colorPicker = document.querySelector('#color-picker');
-const changeColorButton = document.querySelector(".color-button");
+
+// current drawing tool
 let currentTool = changeToBlack;
-// let gridNum = document.querySelector(".slider").value;
+// pixel size description
 let pixelSizeDescription = document.querySelector(".pixel-size");
 
 
-const gridSpace = 200;
-let backgroundColor = "white";
-// let rgbNumber = 255;
-let randomColor;
-
-
- 
-
-
 generateGrid(gridNumber, currentTool);
+
+// function to generate the grid
 function generateGrid(gridNumber, func = "default") {
     for (let i = 0; i < gridNumber; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
         for (let j = 0; j < gridNumber; j++) {
             const div = document.createElement("div");
-            // div.setAttribute("style", `background-color: green; padding: ${gridSpace/gridNumber} px;`)
-            div.style.padding = `${gridSpace/gridNumber}px`;
-            div.style.backgroundColor = backgroundColor;
+            div.setAttribute("style", `background-color: ${gridColorPicker.value}; padding: ${gridSpace/gridNumber}px`);
             div.setAttribute("rgbNumber", "255");
-            // randomColor.addEventListener("click", generateRandomColor);
             
-            pickColor(div, func);
-
-           
-            row.appendChild(div);
+            pickColor(div, func);  // chooses correct drawing tool when creating the grid 
+            row.appendChild(div); // adds div to the row
         }
-        container.appendChild(row);
+        container.appendChild(row); // adds row to the container
     }
 }
 
-function generateRandomColor(e){
-    randomColor = Math.floor(Math.random()*16777215).toString(16);
-    e.target.style.backgroundColor = `#${randomColor}`;
-}
 
+// takes input fro the slider
 slider.oninput = function() {
-    gridNumber = this.value;
-    pixelSizeDescription.textContent = `${gridNumber} x ${gridNumber}`;
+    gridNumber = this.value; // arrow functions do not have this pointer so anonymous function was needed
+    pixelSizeDescription.textContent = `${gridNumber} x ${gridNumber}`; // changes pixel size description
     removeGrid();
     generateGrid(gridNumber, currentTool);
   }
-// gridButton.addEventListener("click", (e) => {
-//     do{
-//         gridNumber = prompt("what number grid would you like? Enter a number between 1 and 64");
-//     }
-//     while (gridNumber < 1 || gridNumber > 64);
-   
-//     removeGrid();
-//     generateGrid(gridNumber, currentTool);
-// });
 
 // function to clear the grid
 clearButton.addEventListener("click", (e) => {
-    // const grid = document.querySelectorAll(".grid");
-    // grid.forEach((element) => {
-    //     element.style.backgroundColor = backgroundColor;
-    //     element.style.rgbNumber = "255";
-    // });
     removeGrid();
     generateGrid(gridNumber, currentTool);
 });
 
 // function to remove all grid elements
-
 function removeGrid() { 
     const count = container.childElementCount;
     for (let i = 0; i < count; i++) { // container.childElementCount is going down when you remove children so variable was needed
@@ -88,83 +66,79 @@ function removeGrid() {
     }
 }
 
+// generates random color or rainbow effect
+function generateRandomColor(e){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.backgroundColor = `#${randomColor}`;
+}
 
-function generateGrayScale(e){
-    
+// function to generate grayscale
+function generateGrayScale(e){    
     rgbNumber = e.target.getAttribute("rgbNumber");
     e.target.style.backgroundColor = `rgb(${rgbNumber}, ${rgbNumber}, ${rgbNumber})`;
     rgbNumber -= 0.1 * 255;
     e.target.setAttribute("rgbNumber", rgbNumber);
 }
 
-function erase(e){
-    e.target.style.backgroundColor = backgroundColor;
+// erase function
+function erase(e){ 
+    e.target.style.backgroundColor = gridColorPicker.value;
 }
 
+// function to change the color of the grid
 function generateGridColor(e){
-    backgroundColor = gridColorPicker.value;
     for (row of container.children)
         for (div of row.children)
-            div.style.backgroundColor = backgroundColor;
+            div.style.backgroundColor = gridColorPicker.value;
 }
 
+// function to change the color of the drawing tool
  function changeColor(e){
     e.target.style.backgroundColor = colorPicker.value;  
  }
 
+ // default drawing tool
  function changeToBlack(e){
     e.target.style.backgroundColor = "black";
 }
 
 
+// button listens for the 4 drawing tool buttons
 rainbowButton.addEventListener("click", () => {
-    for (row of container.children)
-        for (div of row.children){
-            div.removeEventListener("mouseover", currentTool);
-            div.addEventListener("mouseover", generateRandomColor);
-        }
-    currentTool = generateRandomColor;
+    traverseGrid(generateRandomColor);
 });
 
 grayScaleButton.addEventListener("click", () => {
-    for (row of container.children){
-        for (div of row.children){
-            div.removeEventListener("mouseover", currentTool);
-            div.addEventListener("mouseover", generateGrayScale);
-        }
-    }
-    currentTool = generateGrayScale;
+    traverseGrid(generateGrayScale);
 });
 
 
 eraseButton.addEventListener("click", () => {
-    for (row of container.children){
-        for (div of row.children){
-            div.removeEventListener("mouseover", currentTool);
-            div.addEventListener("mouseover", erase);
-        }
-    }
-    currentTool = erase;
+    traverseGrid(erase);
 });
 
 changeColorButton.addEventListener("click", () => {
+    traverseGrid(changeColor);
+});
+
+// function to change the color of the grid
+gridColorButton.addEventListener("click", generateGridColor);
+
+// traverse the grid and add relevant event listeners to every div
+function traverseGrid(func) {
     for (row of container.children){
         for (div of row.children){
             div.removeEventListener("mouseover", currentTool);
-            div.addEventListener("mouseover", changeColor);
+            div.addEventListener("mouseover", func);
         }
     }
-    currentTool = changeColor;
-});
-
-gridColorButton.addEventListener("click", generateGridColor);
-
-
+    currentTool = func;
+}
 
 function pickColor(div, func){
-
+    // switch function for picking correct drawing tools if the user changed grid size or reset the grid
     switch (func) {
-        case generateRandomColor:
+        case generateRandomColor: // adds event listener for every div in the grid
             div.addEventListener("mouseover", generateRandomColor);
             break;
         case generateGrayScale:
